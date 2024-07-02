fc6ce6edd2d7e73e9e5ee6506ea210bc4a208810
Selemin Yuriy <71262890+yuriselemin@users.noreply.github.com>
2024-07-02 14:52:06 +0300

    Add files via upload

--- MOD-6_module_6_3.py
+++ MOD-6_module_6_3.py
@@ -1,55 +1,45 @@
-
-
-# Домашнее задание по теме "Множественное наследование"
-
-
-
-
 class Horse:
-    def __init__(self):
-        self.x_distance = 0
-        self.sound = 'Frrr'
+    def __init__(self, x_distance=0, sound='Frrr'):
+        self.x_distance = x_distance
+        self.sound = sound
 
-    def run(self, dx):
+    def run(self, dx: int):
         self.x_distance += dx
 
+
 class Eagle:
-    def __init__(self):
-        self.y_distance = 0
-        self.sound = 'I train, eat, sleep, and repeat'
+    def __init__(self, y_distance=0, sound='I train, eat, sleep, and repeat'):
+        self.y_distance = y_distance
+        self.sound = sound
 
     def fly(self, dy):
         self.y_distance += dy
 
-class Pegasus(Horse, Eagle):
+
+class Pegasus(Eagle, Horse):
     def __init__(self):
         super().__init__()
         self.x_distance = 0
         self.y_distance = 0
 
-
     def move(self, dx, dy):
         self.run(dx)
         self.fly(dy)
 
-    def get_pos(self):
-        return self.x_distance, self.y_distance
+    def get_pos(self,):
+        return (self.x_distance, self.y_distance)
 
     def voice(self):
         print(self.sound)
 
 
+if __name__ == "__main__":
+    p1 = Pegasus()
 
-p1 = Pegasus()
+    print(p1.get_pos())
+    p1.move(10, 15)
+    print(p1.get_pos())
+    p1.move(-5, 20)
+    print(p1.get_pos())
 
-print(p1.get_pos())
-p1.move(10, 15)
-print(p1.get_pos())
-p1.move(-5, 20)
-print(p1.get_pos())
-
-p1.voice()
-
-
-
-
+    p1.voice()
